7523e8c1f4df53bdaf2b0071945122efae897214
BTea <1248102041@qq.com>
2024-10-07 14:14:20 +0800

    初始化项目

---
+++ src/main/java/com/btea/TreeHoleApplication.java
@@ -0,0 +1,13 @@
+package com.btea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeHoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TreeHoleApplication.class, args);
+    }
+
+}
